0d8d7107d2d50906e5abb8259e6c7d98d6fa54f6
Developer2 <Developer-2@gmail.com>
2020-08-17 15:26:56 +0530

    Initial Commit By Developer-2

--- DemoProject-1/src/com/prudhvi/beans/Demo.java
+++ DemoProject-1/src/com/prudhvi/beans/Demo.java
@@ -5,6 +5,8 @@ public class Demo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to Demo Class");
+		System.out.println("Welcome to Demo Class By Developer-2");
+		
 	}
 
 }
